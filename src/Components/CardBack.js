183e8f4d697b5b428171eef3ddf17971fdf5510d
Brett <brettmc@gmail.com>
2020-02-17 07:05:06 -0500

    little cleaner

--- src/Components/CardBack.js
+++ src/Components/CardBack.js
@@ -11,29 +11,74 @@ const Container = ({ children }) =>
 const Sub = ({ className, children }) => <div className={`${className || ''} card-sub`}>{children}</div>;
 const Sub2 = ({ className, children }) => <div className={`${className || ''} card-sub2`}>{children}</div>;
 
+const Head = ({ name, id }) =>
+    <>
+        <Sub className="back-name">{name ? capitalize(name) : "Loading..."}</Sub>
+        {name && <img className="poke-pic2" alt="critterpic" src={`http://www.pokestadium.com/sprites/xy/${name}.gif`} /> }
+        <Sub className="back-num">#{id || ""}</Sub>
+    </>;
+
+const Moves = ({ move1, move2 }) =>
+    <Sub className="moves">
+        <Sub2>Moves</Sub2>
+        <li className="move-list">{move1}</li>
+        <li className="move-list2">{move2}</li>
+    </Sub>;
+
+
+const Types = ({ types }) =>
+    <Sub className="types type-list">
+        <Sub2 className="type-head">Types</Sub2>
+        {!types ? <></> : types.map((type, i) => <li key={i} className="list-items">{capitalize(type.type.name)}</li>)}
+    </Sub>
+
+
 const Loaded = ({ details }) =>
     <Container>
-        <Sub className="back-name">{capitalize(details.name)}</Sub>
-        <img className="poke-pic2" alt="critterpic" src={`http://www.pokestadium.com/sprites/xy/${details.name}.gif`} />
-        <Sub className="back-num">#{details.id}</Sub>
-        <Sub className="moves">
-            <Sub2>Moves</Sub2>
-            <li className="move-list">{capitalize(details.moves[0].move.name)}</li>
-            <li className="move-list2">{capitalize(details.name === 'ditto' ? '' : details.moves[1].move.name)}</li>
-        </Sub>
-        <Sub className="types type-list">
-            <Sub2 className="type-head">Types</Sub2>
-            {details.types.map((type, i) => <li key={i} className="list-items">{capitalize(type.type.name)}</li>)}
-        </Sub>
+        <Head name={details.name} id={details.id} />
+        <Moves 
+        move1={capitalize(details.moves[0].move.name)}
+        moves2={capitalize(details.name === 'ditto' ? '' : details.moves[1].move.name)}
+        />
+        <Types {...details} />
     </Container>
     ;
 
 
+
+const NotLoaded = () =>
+    <Container>
+        <Head />
+        <Moves />
+        <Types />
+    </Container>
+    ;
+
 const Live = ({ url }) => {
     const [fetchResult, loading] = useFetch(url);
     console.log({ f: "Live", url, fetchResult, loading });
-    return loading ? <Container /> : <Loaded details={fetchResult} />;
+    return loading ? <NotLoaded /> : <Loaded details={fetchResult} />;
 };
 
 export const CardBack = ({ url, live }) =>
-    !live ? <Container /> : <Live url={url} />;
+    !live ? <NotLoaded /> : <Live url={url} />;
+
+
+/*
+const Loaded = ({ details }) =>
+<Container>
+    <Sub className="back-name">{capitalize(details.name)}</Sub>
+    <img className="poke-pic2" alt="critterpic" src={`http://www.pokestadium.com/sprites/xy/${details.name}.gif`} />
+    <Sub className="back-num">#{details.id}</Sub>
+    <Sub className="moves">
+        <Sub2>Moves</Sub2>
+        <li className="move-list">{capitalize(details.moves[0].move.name)}</li>
+        <li className="move-list2">{capitalize(details.name === 'ditto' ? '' : details.moves[1].move.name)}</li>
+    </Sub>
+    <Sub className="types type-list">
+        <Sub2 className="type-head">Types</Sub2>
+        {details.types.map((type, i) => <li key={i} className="list-items">{capitalize(type.type.name)}</li>)}
+    </Sub>
+</Container>
+;
+*/
